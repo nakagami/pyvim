15a4debef314776cae10024666ddfb01139e41ca
Hajime Nakagami <nakagami@gmail.com>
2024-07-07 09:52:57 +0900

    Partially fixed G command.

--- pyvim/key_bindings.py
+++ pyvim/key_bindings.py
@@ -161,6 +161,18 @@ def create_key_bindings(editor):
             new_path, show_in_current_window=True)
         editor.sync_with_prompt_toolkit()
 
+    @kb.add("G", filter=vi_navigation_mode)
+    def to_nth_line(event):
+        # I don't know why, but the default behaviour of the prompt toolkit is that <number>G does not work well.
+        # With this implementation, 1G same as G and moves to the end of the line.
+        # I don't know how to fix it, so please use gg instead of 1G when moving to the first line
+        buf = event.current_buffer
+        count = (buf.document.line_count if event.arg == 1 else event.arg -1) - buf.document.cursor_position_row
+        if count > 0:
+            buf.auto_down(count=count, go_to_start_of_line_if_history_changes=True)
+        elif count < 0:
+            buf.auto_up(count=-count, go_to_start_of_line_if_history_changes=True)
+
     return kb
 
 
