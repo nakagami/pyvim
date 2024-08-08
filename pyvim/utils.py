9c763ff1fe6cf0933dafa9d64c33ac940d1fceb6
Hajime Nakagami <nakagami@gmail.com>
2024-08-08 17:24:22 +0900

    word boundary regex #39

--- pyvim/utils.py
+++ pyvim/utils.py
@@ -3,7 +3,7 @@ import re
 
 def re_finditer(sub, text, flags):
     try:
-        iterator = re.finditer(sub, text, flags)
+        iterator = re.finditer(sub.replace(r"\<", r"\b(?=\w)").replace(r"\>", r"\b(?<=\w)"), text, flags)
     except re.error:
         iterator = re.finditer(re.escape(sub), text, flags)
 
