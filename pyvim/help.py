5b85e71c1a2f72bc29c16b33d0b6d2b2a1ca3f8a
Hajime Nakagami <nakagami@gmail.com>
2024-10-07 15:48:32 +0900

    fix typo

--- pyvim/help.py
+++ pyvim/help.py
@@ -18,8 +18,8 @@ When editing Python with Jedi enabled, an autocomplete window appears and follow
 
 - Ctrl+n: select next candidate
 - Ctrl+p: select previsous candidate
-- Ctrl+g: accept and close autocompletion window
-- Ctrl+e: cancen completion and close autocompletion window
+- Ctrl+g: accept completion and close autocompletion window
+- Ctrl+e: cancel completion and close autocompletion window
 
 This key binding may change in the future.
 
