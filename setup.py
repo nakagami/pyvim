1922249d38a3ea0f77cad5666454b08bddaca847
Hajime Nakagami <nakagami@gmail.com>
2025-04-15 19:56:00 +0900

    prompt_toolkit==3.0.51

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     long_description=long_description,
     packages=find_packages("."),
     install_requires=[
-        "prompt_toolkit==3.0.50",
+        "prompt_toolkit==3.0.51",
         "pyflakes",  # For Python error reporting.
         "jedi",  # For Python autocompletion
         "pygments",  # For the syntax highlighting.
